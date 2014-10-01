1a802d59870cbe4546833e919fd58c7c3a7f5245
Matt Taylor <Matt@Navgear.com>
2014-10-01 11:13:17 -0500

    rails generate delayed_job:active_record

--- Project/db/schema.rb
+++ Project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141001153827) do
+ActiveRecord::Schema.define(:version => 20141001161105) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,22 @@ ActiveRecord::Schema.define(:version => 20141001153827) do
     t.integer "category_id"
     t.integer "listing_id"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0, :null => false
+    t.integer  "attempts",   :default => 0, :null => false
+    t.text     "handler",                   :null => false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "listings", :force => true do |t|
     t.integer  "user_id"
